c3dadd239ff58d9655b2abf4b1d441e040c6c579
Austin Polk <austinpolk32@gmail.com>
2024-10-23 21:32:30 -0500

    Formulate a process for classifying movies based on keywords in the plot description, begin developing the final application

---
+++ movie_reviews.py
@@ -0,0 +1,11 @@
+from typing import Literal
+import numpy as np
+
+def get_plot_keywords(movie_plots: dict[])
+
+def train_keyword_embeddings(keywords: list[str], method: Literal['word2vec', 'genres']) -> dict[str, np.ndarray]:
+    pass
+
+def train_user_model():
+    pass
+
