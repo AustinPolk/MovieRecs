8ff1d582d5dbc704f74edb62325512d98b970862
Austin Polk <austinpolk32@gmail.com>
2024-11-06 22:53:49 -0600

    Add Describe function

--- recommender.py
+++ recommender.py
@@ -1,12 +1,17 @@
 from trainer import Trainer
 from loader import Loader
 import pickle
+import numpy as np
 
 class Recommender:
     def __init__(self):
         self.Trainer: Trainer = None
         self.Loader: Loader = None
         self.EncodedMovies: dict = {}
+        self.MovieTitles: dict
+
+        with open("cache\\movies_by_id.bin", "rb") as f:
+            self.MovieTitles = pickle.load(f)
 
     def Save(self):
         with open("cache\\trainer.bin", "wb+") as f:
@@ -37,4 +42,53 @@ class Recommender:
         for id, plot_str in cached_plots.items():
             self.EncodedMovies[id] = self.Trainer.plot_autoencoding(plot_str)
 
-        
+    # return the similarity between two vectors using the given method, the higher the more similar
+    def _encoding_similarity(self, e_s: np.ndarray, e_t: np.ndarray, method: str):
+        smoothing = 0.001   # ensures no division by zero errors
+        if method == "euclid":
+            dist_squared = smoothing
+            for i in range(e_s.shape[0]):
+                dist_squared += (e_s[i] - e_t[i])**2
+            return dist_squared**(-1/2)
+        if method == "euclid_squared":
+            dist_squared = smoothing
+            for i in range(e_s.shape[0]):
+                dist_squared += (e_s[i] - e_t[i])**2
+            return dist_squared**(-1)
+        if method == "average":
+            total_deviation = smoothing
+            for i in range(e_s.shape[0]):
+                total_deviation += abs(e_s[i] - e_t[i])
+            return (total_deviation / e_s.shape[0])
+        if method == "maximum":
+            max_deviation = -1
+            for i in range(e_s.shape[0]):
+                dev = abs(e_s[i] - e_t[i])
+                if dev > max_deviation:
+                    max_deviation = dev
+            return 1 / (max_deviation + smoothing)
+        if method == "cosine":
+            dot_prod = 0
+            s_mag = smoothing
+            t_mag = smoothing
+            for i in range(e_s.shape[0]):
+                dot_prod += e_s[i] * e_t[i]
+                s_mag += e_s[i] * e_s[i]
+                t_mag += e_t[i] * e_t[i]
+            return dot_prod / (s_mag * t_mag)**(1/2)
+
+    def Describe(self, description: str, similarity_method: str, top_n: int = 5): 
+        described = self.Trainer.plot_autoencoding(description)
+
+        sim_scores = {}
+        for id, encoding in self.EncodedMovies.items():
+            similarity = self._encoding_similarity(described, encoding, method=similarity_method)
+            sim_scores[id] = similarity
+
+        ranked = list(self.EncodedMovies.keys())
+        ranked = sorted(ranked, key=lambda x: sim_scores[x], reverse=True)
+
+        top_ids = ranked[:top_n]
+        top_titles = [self.MovieTitles[id] for id in top_ids]
+
+        return top_titles
