2537f30fb17779808c15337917512a9f32920ebb
Austin Polk <austinpolk32@gmail.com>
2024-11-14 10:19:15 -0600

    add to loader

--- loader.py
+++ loader.py
@@ -4,6 +4,13 @@ class Loader:
     def __init__(self):
         pass
 
+    def dopickle(filepath: str, thing):
+        try:
+            with open(filepath, "wb+") as f:
+                p = pickle.dump(thing, f)
+        except:
+            return
+
     def _unpickle(self, filepath: str):
         p = None
         try:
@@ -13,6 +20,18 @@ class Loader:
             p = None
         return p
 
+    def load(self, from_file: str):
+        thing = self._unpickle(f"cache\\{from_file}.bin")
+        return thing
+    
+    def save(self, thing, to_file: str):
+        self.dopickle(f"cache\\{to_file}.bin", thing)
+        return
+
     def load_cached_plots(self):
         plots_by_id = self._unpickle("cache\\plots_by_id.bin")
         return plots_by_id
+    
+    def load_cached_titles(self):
+        movies_by_id = self._unpickle("cache\\movies_by_id.bin")
+        return movies_by_id
