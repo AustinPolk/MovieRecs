70d9131698ff185cf185b1033cf08c452ec08176
Austin Polk <austinpolk32@gmail.com>
2024-11-15 17:41:47 -0600

    change the loader to be static instead of based on an object

--- loader.py
+++ loader.py
@@ -1,37 +1,40 @@
 import pickle
+import os
 
-class Loader:
-    def __init__(self):
-        pass
+def __init__(self):
+    pass
 
-    def dopickle(self, filepath: str, thing):
-        try:
-            with open(filepath, "wb+") as f:
-                p = pickle.dump(thing, f)
-        except:
-            return
-
-    def _unpickle(self, filepath: str):
-        p = None
-        try:
-            with open(filepath, "rb") as f:
-                p = pickle.load(f)
-        except:
-            p = None
-        return p
-
-    def load(self, from_file: str):
-        thing = self._unpickle(f"cache\\{from_file}.bin")
-        return thing
-    
-    def save(self, thing, to_file: str):
-        self.dopickle(f"cache\\{to_file}.bin", thing)
+def dopickle(self, filepath: str, thing):
+    try:
+        with open(filepath, "wb+") as f:
+            p = pickle.dump(thing, f)
+    except:
         return
 
-    def load_cached_plots(self):
-        plots_by_id = self._unpickle("cache\\plots_by_id.bin")
-        return plots_by_id
-    
-    def load_cached_titles(self):
-        movies_by_id = self._unpickle("cache\\movies_by_id.bin")
-        return movies_by_id
+def _unpickle(self, filepath: str):
+    p = None
+    try:
+        with open(filepath, "rb") as f:
+            p = pickle.load(f)
+    except:
+        p = None
+    return p
+
+def exists(self, file: str):
+    return os.path.exists(f"cache\\{file}.bin")
+
+def load(self, from_file: str):
+    thing = self._unpickle(f"cache\\{from_file}.bin")
+    return thing
+
+def save(self, thing, to_file: str):
+    self.dopickle(f"cache\\{to_file}.bin", thing)
+    return
+
+def load_cached_plots(self):
+    plots_by_id = self._unpickle("cache\\plots_by_id.bin")
+    return plots_by_id
+
+def load_cached_titles(self):
+    movies_by_id = self._unpickle("cache\\movies_by_id.bin")
+    return movies_by_id
