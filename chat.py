d2355e7ffdb2b996656e115ad1b91836bca08eae
Austin Polk <austinpolk32@gmail.com>
2024-12-02 13:50:53 -0600

    Add basic user response handling

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 from service import UserPreferences, MovieRecommendation, MovieService
 from transformers import pipeline
 import random
+import spacy
 
 class RequestChecklist:
     def __init__(self):
@@ -95,20 +96,133 @@ class Chatter:
             'Outro': 'I hope that my recommendations will be of service to you. Come back soon!'
         }
         self.Paraphraser = pipeline("text2text-generation", model="Vamsi/T5_Paraphrase_Paws")
+        self.Language = spacy.load("en_core_web_lg")
         self.Checklist = RequestChecklist()
         self.MovieService = MovieService()
         self.UserPreferences = UserPreferences()
 
-    def get_and_parse_user_response(self):
-        pass
+    def get_liked_movies(self, user_response):
+        liked_movies = []
+        for entity in user_response.ents:
+            if entity.label_ == 'WORK_OF_ART':
+                liked_movies.append(entity.text)
+        return liked_movies if liked_movies else None
 
-    # detect if the user wants a recommendation given the current information
+    def get_described_movies(self, user_response):
+        described = []
+        for sent in user_response.sents:
+            described.append(sent.text)
+        return described if described else None
+
+    def get_liked_directors(self, user_response):
+        liked_directors = []
+        for entity in user_response.ents:
+            if entity.label_ == 'PERSON':
+                liked_directors.append(entity.text)
+        return liked_directors if liked_directors else None
+
+    def get_liked_genres(self, user_response):
+        liked_genres = []
+        for token in user_response:
+            if token.pos_ in ['NOUN', 'ADJ']:
+                liked_genres.append(token.text)
+        return liked_genres if liked_genres else None
+
+    def get_liked_actors(self, user_response):
+        liked_actors = []
+        for entity in user_response.ents:
+            if entity.label_ == 'PERSON':
+                liked_actors.append(entity.text)
+        return liked_actors if liked_actors else None
+
+    def get_time_period(self, user_response):
+        before = None
+        after = None
+        for token in user_response:
+            if token.lemma_.lower() == 'before' and not before:
+                before = True
+            elif token.lemma_.lower() == 'after' and not after:
+                after = True
+            elif token.lemma_.lower() == 'from' and not after:
+                after = True
+            elif token.lemma_.lower() == 'to' and not before:
+                before = True
+            elif token.like_num and before:
+                before = int(token.text)
+            elif token.like_num and after:
+                after = int(token.text)
+        return (before, after)
+
+    def get_american_films(self, user_response):
+        for token in user_response:
+            if token.lemma_.lower() == 'no':
+                return False
+        return True
+
+    def get_foreign_films(self, user_response):
+        for token in user_response:
+            if token.lemma_.lower() == 'no':
+                return False
+        return True
+
+    def get_bollywood_films(self, user_response):
+        for token in user_response:
+            if token.lemma_.lower() == 'no':
+                return False
+        return True
+
+    def get_and_parse_user_response(self, next_request: str):
+        self.generate_prompt(next_request)
+        
+        response = input(">>> ")
+
+        if self.detect_off_topic(response):
+            self.generate_prompt('Redirect')
+            return self.get_and_parse_user_response(next_request)
+        elif self.detect_refusal(response) and next_request not in ['AmericanFilms', 'ForeignFilms', 'BollywoodFilms']:
+            self.generate_prompt('SaidNo')
+            return False
+        elif self.detect_ready_for_recommendation(response):
+            return True
+        
+        tokenized_response = self.Language(response)
+
+        if next_request == 'LikedMovies':
+            self.UserPreferences.KnownLikedTitles = self.get_liked_movies(tokenized_response)
+        if next_request == 'DescribedMovies':
+            self.UserPreferences.DescribedPlots = self.get_described_movies(tokenized_response)
+        if next_request == 'LikedDirectors':
+            self.UserPreferences.Directors = self.get_liked_directors(tokenized_response)
+        if next_request == 'LikedGenres':
+            self.UserPreferences.Genres = self.get_liked_genres(tokenized_response)
+        if next_request == 'LikedActors':
+            self.UserPreferences.Actors = self.get_liked_actors(tokenized_response)
+        if next_request == 'TimePeriod':
+            self.UserPreferences.MoviesBefore, self.UserPreferences.MoviesAfter = self.get_time_period(tokenized_response)
+        if next_request == 'AmericanFilms':
+            self.UserPreferences.AllowAmericanOrigin = self.get_american_films(tokenized_response)
+        if next_request == 'ForeignFilms':
+            self.UserPreferences.AllowOtherOrigin = self.get_foreign_films(tokenized_response)
+        if next_request == 'BollywoodFilms':
+            self.UserPreferences.AllowBollywoordOrigin = self.get_bollywood_films(tokenized_response)
+
+        return False
+
+    # detect if the user has gone off topic (currently ignore this one)
+    def detect_off_topic(self, user_input: str):
+        return False
+
+    # detect if the user wants a recommendation given the current information (currently ignore this one)
     def detect_ready_for_recommendation(self, user_input: str):
-        pass
+        return False
 
     # detect if the user declines to answer a question
     def detect_refusal(self, user_input: str):
-        pass
+        tokenized = self.Language(user_input)
+        for token in tokenized:
+            if token.lemma_.lower() == 'no':
+                return True
+        return False
 
     def generate_prompt(self, part_of_conversation: str):
         original_prompt = self.Prompts[part_of_conversation]
@@ -136,8 +250,9 @@ class Chatter:
 
         while not self.Checklist.done_yet():
             next_request = self.Checklist.next_request()
-            self.generate_prompt(next_request)
-            self.get_and_parse_user_response()
+            breakout = self.get_and_parse_user_response(next_request)
+            if breakout:
+                break
 
         waiting = self.Checklist.next_request()
         self.generate_prompt(waiting)
